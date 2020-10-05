5e9adaf45a7dbd2e6b2e2cf63eb61ddc0a030b2c
vagrant <vagrant@localhost.localdomain>
2020-10-05 15:08:55 +0000

    fix

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,25 @@
 class Admin::GenresController < ApplicationController
 
   def index
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice]="successfully"
+       redirect_to request.referer
+    else
+      @genres=Genre.all
+      redirect_to request.referer
+    end
   end
 
   def edit
   end
 
   def update
+
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,5 +4,5 @@ class Genre < ApplicationRecord
   validates :is_active, presence: true
 
   has_many :products, dependent: :destroy
-
+  boolean is_active:{有効：false,無効:true}
 end
