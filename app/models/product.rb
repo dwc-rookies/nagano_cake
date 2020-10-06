1b2cf854feee5855ec802a5717e5694504405daf
vagrant <vagrant@localhost.localdomain>
2020-10-06 19:01:59 +0900

    [Add]adminå商側商å側商品画面

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,12 @@ class Product < ApplicationRecord
   validates :tax_excluded_price, presence: true
   validates :description, presence: true
   validates :image_id, presence: true
-  validates :is_active, presence: true
+  validates :is_active, inclusion: {in: [true, false]}
 
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :ordered_products, dependent: :destroy
+
+  attachment :image
 
 end
