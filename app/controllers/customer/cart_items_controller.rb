e3eb81203484bcec74c5a4acbf548ae027f067d2
vagrant <vagrant@localhost.localdomain>
2020-10-07 15:58:48 +0900

    [Add]カート機能追加

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Customer::CartItemsController < ApplicationController
 
   def index
+    @cart_items = current_customer.cart_items
   end
 
   def create
@@ -10,9 +11,21 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def delete_all
+		@cart_items =  current_customer.cart_items
+		@cart_items.delete_all
+    redirect_to cart_items_path
   end
+
+private
+
+def cart_params
+  params.require(:cart).permit(:quantity,:product_id)
+end
 
 end
