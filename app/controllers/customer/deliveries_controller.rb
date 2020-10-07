280a9b397d8e5f472de6def8633d02b936984c71
ohnoshobu <ohno56sent@gmail.com>
2020-10-07 15:52:40 +0900

    配送関連機能実装

--- app/controllers/customer/deliveries_controller.rb
+++ app/controllers/customer/deliveries_controller.rb
@@ -2,11 +2,13 @@ class Customer::DeliveriesController < ApplicationController
 
   def index
     @delivery = Delivery.new
-    @deliveries = Delivery.all
+   
+    @deliveries = current_customer.deliveries
   end
 
   def create
     @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
     @delivery.save
     redirect_to deliveries_path(@delivery.id)
   end
@@ -16,7 +18,7 @@ class Customer::DeliveriesController < ApplicationController
   end
 
   def update
-    @delivery = List.find(params[:id])
+    @delivery = Delivery.find(params[:id])
     @delivery.update(delivery_params)
     redirect_to deliveries_path(@delivery.id)
   end
@@ -24,7 +26,7 @@ class Customer::DeliveriesController < ApplicationController
   def destroy
     delivery = Delivery.find(params[:id])
     delivery.destroy
-    redirect_to deliveries_path(@delivery.id)
+    redirect_to deliveries_path
   end
 
   private
