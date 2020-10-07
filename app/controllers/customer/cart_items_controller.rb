6737771b2a40e6a06cf23168f9c498941568f7ff
vagrant <vagrant@localhost.localdomain>
2020-10-07 17:37:06 +0900

    [Add]カート機è機能

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -5,9 +5,26 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id]).present?
+      cart_item = current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
+      flash[:notice] = "Item was successfully added to cart."
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      flash[:notice] = "You have creatad cart_item successfully."
+      redirect_to cart_items_path
+    else
+      redirect_back(fallback_location: products_path)
+    end
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -17,15 +34,15 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def delete_all
-		@cart_items =  current_customer.cart_items
+		@cart_items = current_customer.cart_items
 		@cart_items.delete_all
     redirect_to cart_items_path
   end
 
 private
 
-def cart_params
-  params.require(:cart).permit(:quantity,:product_id)
+def cart_item_params
+  params.require(:cart_item).permit(:quantity, :product_id, :customer_id)
 end
 
 end
