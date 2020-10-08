271322ae32d851bafcc607fc5987712d468a09fb
vagrant <vagrant@localhost.localdomain>
2020-10-08 09:44:34 +0000

    fix

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,16 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module NaganoCake
   class Application < Rails::Application
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+      html_tag
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -16,4 +20,7 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+  
 end
+
+
