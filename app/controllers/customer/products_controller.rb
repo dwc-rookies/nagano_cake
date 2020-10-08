98520874026ebaa38bf6f6dce072d00478d0ebb7
vagrant <vagrant@localhost.localdomain>
2020-10-08 12:42:07 +0900

    [Add]ジャンル検索機能

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,12 +1,19 @@
 class Customer::ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.where(is_active: true)
+    @genres = Genre.all
+    genre_id = params[:genre_id] #ジャンル検索用のlink_toメソッドには、ジャンルidパラメータを付加している。
+    if genre_id.present? #もしジャンルidパラメータが存在するなら、そのジャンルの製品のみを抽出する。
+      @products = @products.where(genre_id: genre_id)
+    else
+    end
   end
 
   def show
     @product = Product.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
 end
--- db/migrate/20201007040203_change_column_to_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToNull < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :products, :image_id, true
-  end
-end
