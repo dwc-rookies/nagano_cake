015526267f6ad9e11ba072ce03fafb670c09fe6d
vagrant <vagrant@localhost.localdomain>
2020-10-08 18:59:07 +0900

    [Add]管理者側注文画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,13 +2,19 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin_admin!
 
   def index
-    @order = Order.all.order(created_at: "DESC")
+    @orders = Order.all.order(created_at: "DESC")
   end
 
   def show
+    @order = Order.find(params[:id])
+    @ordered_product = OrderedProduct.all.includes(:rooms)
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    flash[:notice] = "You have updated order successfully."
+    redirect_back(fallback_location: admin_orders_path)
   end
 
   private
--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -2,7 +2,7 @@ class Customer::ProductsController < ApplicationController
 
   def index
     @products = Product.where(is_active: true)
-    @genres = Genre.all
+    @genres = Genre.where(is_active: true)
     genre_id = params[:genre_id] #ジャンル検索用のlink_toメソッドには、ジャンルidパラメータを付加している。
     if genre_id.present? #もしジャンルidパラメータが存在するなら、そのジャンルの製品のみを抽出する。
       @products = @products.where(genre_id: genre_id)
