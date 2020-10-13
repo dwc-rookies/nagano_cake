b9baf5ea2351bd78d8223302bbf97d5e75f868f8
vagrant <vagrant@localhost.localdomain>
2020-10-13 15:31:04 +0900

    [Fix]バグ修正

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,8 +1,9 @@
 class Customer::OrdersController < ApplicationController
   before_action :authenticate_customer!
+  before_action :cart_item_check,{only: [:new, :confirm, :create]}
 
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders.order(created_at: "DESC")
   end
 
   def show
@@ -22,14 +23,13 @@ class Customer::OrdersController < ApplicationController
     @order =  current_customer.orders.build(order_params)
     @cart_items = current_customer.cart_items
     if request.post? then
-
-
       if params[:order][:pay] == "credit_card"
         @pay = "クレジットカード"
       else
         @pay = "銀行振込"
       end
     end
+    session[:new_delivery] = nil
     case params[:delivery_address_type]
     when "ご自身の住所"
       @order.postcode = current_customer.postcode
@@ -40,39 +40,69 @@ class Customer::OrdersController < ApplicationController
       @order.address = Delivery.find(set_delivery[:id]).address
       @order.name = Delivery.find(set_delivery[:id]).name
     when "新しいお届け先"
+      if @order.postcode.blank? or @order.address.blank? or @order.name.blank?
+        @order = Order.new
+        @delivery = Delivery.new
+        render 'new'
+      else
+        session[:new_delivery] = true
+        @delivery = Delivery.new
+      end
     end
   end
 
   def create
-     @order = Order.new(order_params)
-     @cart_items = current_customer.cart_items
-     @order.save
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
+    @order.save
 
       current_customer.cart_items.each do |ordered_product|
-            @ordered_product = OrderedProduct.new(
-            order_id: @order.id,
-            product_id: ordered_product.product.id,
-            purchase_money: ordered_product.product.tax_excluded_price,
-            quantity: ordered_product.quantity,
-            )
-            @ordered_product.save!
-        end
+        @ordered_product = OrderedProduct.new(
+          order_id: @order.id,
+          product_id: ordered_product.product.id,
+          purchase_money: ordered_product.product.tax_excluded_price,
+          quantity: ordered_product.quantity,
+        )
+        @ordered_product.save!
+      end
 
-     @cart_items.destroy_all
-     redirect_to order_completed_path
+    if session[:new_delivery]
+      @delivery = Delivery.new(
+        customer_id: current_customer.id,
+        postcode: @order.postcode,
+        address: @order.address,
+        name: @order.name
+      )
+      @delivery.save
+      session[:new_delivery] = nil
+    end
+    @cart_items.destroy_all
+    session[:order_completed] = true
+    redirect_to order_completed_path
   end
 
   def complete
+    if session[:order_completed] 
+      session[:order_completed] = nil
+    else
+      redirect_to root_path
+    end
   end
 
   private
 
-   def order_params
-     params.require(:order).permit(:pay, :postcode, :address, :name, :postage, :charge, :customer_id)
-   end
+  def order_params
+    params.require(:order).permit(:pay, :postcode, :address, :name, :postage, :charge, :customer_id)
+  end
 
-   def set_delivery
+  def set_delivery
     params.require(:order).require(:delivery).permit(:id)
   end
 
+  def cart_item_check
+    cart_items = current_customer.cart_items
+    if cart_items.blank?
+      redirect_to cart_items_path
+    end
+  end
 end
